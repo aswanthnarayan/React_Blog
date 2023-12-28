488c17528c3adc735e8ba0d289d7e5a724611017
aswanthnarayan <aswanthnarayanan@gmail.com>
2023-12-28 16:23:39 +0530

    created the posts section and add  mobile responsiveness

---
+++ react-blog-app/src/Components/Posts/Post/Post.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Post.module.scss'
+import postImg from '../../../assets/singlepost.jpeg'
+
+const Post = () => {
+  return (
+    <div className={styles.post}>
+        <img src={postImg} alt="postImg" />
+        <div className={styles.postDetails}>
+            <div className={styles.postcategory}>
+                <p>Music</p>
+                <p>Life</p>
+            </div>
+            <p className={styles.postTitle}>Lorem ipsum dolor sit amet.</p>
+            <p className={styles.timeStamp}> 1 hour ago</p>
+            <p className={styles.postContent}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque in aut quaerat id, reprehenderit iste ex voluptates culpa ut, autem nisi, eveniet error sit. Amet, est incidunt et distinctio dolores neque ipsa ratione! Nostrum adipisci quasi voluptates, incidunt delectus, reiciendis a nesciunt voluptas saepe minima harum nam. Sapiente enim iusto possimus labore porro nisi doloremque libero, animi nobis consequatur voluptatibus corporis culpa mollitia magni debitis non atque sint repudiandae facilis consectetur eveniet asperiores aspernatur accusamus!</p>
+        </div>
+    </div>
+  )
+}
+
+export default Post
--- react-blog-app/src/Components/Posts/Posts.jsx
+++ react-blog-app/src/Components/Posts/Posts.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './Posts.module.scss'
+import Post from './Post/Post'
 
 const Posts = () => {
   return (
-    <div className={styles.posts}>Posts</div>
+    <div className={styles.posts}>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
   )
 }
 
