55b9acf0c897b0682b1f33ee29fef140e6dd53c1
aswanthnarayan <aswanthnarayanan@gmail.com>
2024-01-05 17:15:49 +0530

    connected to mongo server, created first model for users

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
+//initialize express
 const express = require('express');
 const app= express();
+//initialize dotenv
+const dotenv = require('dotenv');
+dotenv.config()
+//initialize mongoose and connected to server
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log('connected to mongo'))
+.catch((err)=>{
+    console.log(err)
+});
 
-app.use('/',(req,res)=>{
-    res.send('hi')
-})
 
 app.listen('5500',()=>{
     console.log('server initialized')
